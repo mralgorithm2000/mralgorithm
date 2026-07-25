2ebfe652a3898a4acf635f2ea3c8d1fa4cbfd981
mralgorithm <mralgorithm.2000@gmail.com>
2026-07-25 11:28:38 +0330

    test

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,10 @@ Route::get('/payment/verify', function () {
     return view('payment.verify');
 })->name('payment.verify');
 
+Route::get('/order/status',[OrderStatusController::class,'index'])->name('order.status');
+
+
 Route::get('/order/vn', function () {
     return view('order.vn');
 })->name('order.vn');
 
-Route::get('/order/status',[OrderStatusController::class,'index'])->name('order.status');
-
-
-Route::get('/re', function () {
-
-return response()->json($response->json());
-})->name('payment.verify');
-
-
-
-
-🦄
