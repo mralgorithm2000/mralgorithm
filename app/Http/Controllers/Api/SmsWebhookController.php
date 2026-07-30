1b4b95a3278fc19040e5b3a9f87dfa8104550f57
mralgorithm <mralgorithm.2000@gmail.com>
2026-07-30 16:00:10 +0330

    test

--- app/Http/Controllers/Api/SmsWebhookController.php
+++ app/Http/Controllers/Api/SmsWebhookController.php
@@ -9,25 +9,29 @@ use App\Models\NumberOrder;
 use App\Services\SmsCodeBroadcastService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SmsWebhookController extends Controller
 {
 
     public function smscodex(Request $request, SmsCodeBroadcastService $smsCodeBroadcastService): JsonResponse
     {
-        $order = NumberOrder::where('phone_number',$request->post('phone'))
-        ->where('country_code',$request->post('country'))
-        ->firstOrFail();
-
-        $smsCode = $request->post('sms_code');
-
-        $order->update([
-            'sms_code' => $smsCode,
-            'status' => NumberOrderStatus::RECEIVED->value,
+        Log::info('request',[
+            'req' => $request->all()
         ]);
+        // $order = NumberOrder::where('phone_number',$request->post('phone'))
+        // ->where('country_code',$request->post('country'))
+        // ->firstOrFail();
 
-        $smsCodeBroadcastService->broadcast($order->id, $smsCode);
+        // $smsCode = $request->post('sms_code');
 
-        return response()->json(['success' => true]);
+        // $order->update([
+        //     'sms_code' => $smsCode,
+        //     'status' => NumberOrderStatus::RECEIVED->value,
+        // ]);
+
+        // $smsCodeBroadcastService->broadcast($order->id, $smsCode);
+
+        // return response()->json(['success' => true]);
     }
 }
