94edbd358e0d8de9476589f0e696c25a3ad32537
mralgorithm <mralgorithm.2000@gmail.com>
2026-07-31 08:40:21 +0330

    test

--- app/Services/SmsCodexService.php
+++ app/Services/SmsCodexService.php
@@ -47,7 +47,7 @@ class SmsCodexService
                     'extras' => [
                         'priority' => 'quality',
                     ],
-                    "callback_url": "https://my.mralgorithm.ru/api/sms/webhook/smscodex",
+                    "callback_url" => "https://my.mralgorithm.ru/api/sms/webhook/smscodex",
                     // 'idempotency_key' => (string) $order_id,
                 ]
             );
