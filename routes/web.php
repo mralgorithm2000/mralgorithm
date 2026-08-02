92a7be77c2ed6a4c5ed4f356d4f09fc3dcd92e6e
mralgorithm <mralgorithm.2000@gmail.com>
2026-08-02 22:12:27 +0330

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\OrderStatusController;
+use App\Models\NumberOrder;
 use App\Services\DigisellerService;
+use App\Services\SmsCodeBroadcastService;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -17,3 +19,24 @@ Route::get('/order/status',[OrderStatusController::class,'index'])->name('order.
 Route::get('/order/vn', function () {
     return view('order.vn');
 })->name('order.vn');
+
+Route::get('/test/sms-code-broadcast', function (SmsCodeBroadcastService $smsCodeBroadcastService) {
+    $order = NumberOrder::query()->latest('id')->first();
+
+    if (! $order) {
+        return response()->json([
+            'success' => false,
+            'message' => 'No number order found.',
+        ], 404);
+    }
+
+    $smsCode = '123465789';
+
+    $smsCodeBroadcastService->broadcast($order->id, $smsCode);
+
+    return response()->json([
+        'success' => true,
+        'order_id' => $order->id,
+        'sms_code' => $smsCode,
+    ]);
+})->name('test.sms-code-broadcast');
